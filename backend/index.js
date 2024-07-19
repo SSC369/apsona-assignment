f1db6c97e245c5fa9c0973774e958032a7c1c3cf
SSC369 <sai9821c@gmail.com>
2024-07-19 11:51:16 +0530

    apson

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/note", noteRoutes);
-
+app.get("/", () => {
+  res.json("Its working");
+});
 app.listen(3000, () => {
   console.log("Server is running on 3000 port");
 });
